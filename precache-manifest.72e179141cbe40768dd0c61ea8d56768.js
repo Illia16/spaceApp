f834c7ad0264486a76c756cab379de741b2897ea
Illia <illia2143@gmail.com>
2020-08-26 19:24:48 -0400

    Updates

---
+++ precache-manifest.72e179141cbe40768dd0c61ea8d56768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12595007f6148461da1be43cfd0e883d",
+    "url": "/spaceApp/index.html"
+  },
+  {
+    "revision": "876f57ed98f94972bdd1",
+    "url": "/spaceApp/static/css/main.e86be710.chunk.css"
+  },
+  {
+    "revision": "4aace0203bd43e615e15",
+    "url": "/spaceApp/static/js/2.73e67e70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spaceApp/static/js/2.73e67e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876f57ed98f94972bdd1",
+    "url": "/spaceApp/static/js/main.978fa653.chunk.js"
+  },
+  {
+    "revision": "f9add5b5a9cf8f883f43",
+    "url": "/spaceApp/static/js/runtime-main.e269b191.js"
+  }
+]);
