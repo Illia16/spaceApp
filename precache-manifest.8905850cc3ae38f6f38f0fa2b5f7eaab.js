75a75fb046929934b7382ccd936fa3a2eea98026
Illia <illia2143@gmail.com>
2020-09-26 00:35:19 -0400

    Updates

--- precache-manifest.8905850cc3ae38f6f38f0fa2b5f7eaab.js
+++ precache-manifest.8905850cc3ae38f6f38f0fa2b5f7eaab.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4265cdea084ef12d7679778018ceee81",
+    "revision": "a4b520b1d040d67fc8fb42f63e4f7af6",
     "url": "/spaceApp/index.html"
   },
   {
-    "revision": "2e54a88ed5cf85f2ba6f",
-    "url": "/spaceApp/static/css/main.d5069281.chunk.css"
+    "revision": "a8089954d2914aa7a31c",
+    "url": "/spaceApp/static/css/main.b8f11ce4.chunk.css"
   },
   {
-    "revision": "6ef5ca9dbb91ebb3c69c",
-    "url": "/spaceApp/static/js/2.3d4e13f4.chunk.js"
+    "revision": "44fab312a031fe405791",
+    "url": "/spaceApp/static/js/2.55d5bd11.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/spaceApp/static/js/2.3d4e13f4.chunk.js.LICENSE.txt"
+    "url": "/spaceApp/static/js/2.55d5bd11.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e54a88ed5cf85f2ba6f",
-    "url": "/spaceApp/static/js/main.ade3b0e9.chunk.js"
+    "revision": "a8089954d2914aa7a31c",
+    "url": "/spaceApp/static/js/main.5b38965d.chunk.js"
   },
   {
     "revision": "f9add5b5a9cf8f883f43",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spaceApp/precache-manifest.69c6a95420c54e8c0c921c4b764a1221.js"
+  "/spaceApp/precache-manifest.8905850cc3ae38f6f38f0fa2b5f7eaab.js"
 );
 
 self.addEventListener('message', (event) => {
