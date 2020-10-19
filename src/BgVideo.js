c48df42d3f6bea18f02be7121ae4c23b9d783c57
Illia <illia2143@gmail.com>
2020-10-18 20:45:53 -0400

    background toggle added with npm Switch used

--- src/BgVideo.js
+++ src/BgVideo.js
@@ -1,29 +1,36 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import "./styles/app.scss";
 import Video from "./img/BgVideo2.mp4";
+import Switch from "react-switch";
 
 const BgVideo = () => {
-    const spaceVideoRef= useRef();
+    const spaceVideoRef = useRef();
     const updateSpeed = () => { spaceVideoRef.current.playbackRate = .8; };
 
-    return(
-            <video
-                autoPlay
-                loop
-                muted
-                ref={ spaceVideoRef }
-                onCanPlay={ () => updateSpeed() }
+    // hook to toogle between background VIDEO and PICTURE (so that the user can read without distraction)
+    const [bgVideo, toggleBg] = useState(true);
 
-                style={{
-                    position: "fixed",
-                    width: "100%",
-                    height: "100vh",
-                    objectFit: "cover",
-                    zIndex: "-10",
-                }}
-            >
+    return(
+        <div>
+            {
+            bgVideo ?
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    ref={ spaceVideoRef }
+                    onCanPlay={ () => updateSpeed() }
+                    className="bg bgVideo"
+                >
                 <source src={Video} type="video/mp4"/>
-            </video>
+                </video>
+                : 
+                <div className="bg bgPicture"></div>
+            }
+            <label htmlFor="toggleBg" className="srOnly">toggle background from video to picture</label>
+            <Switch onChange={e => toggleBg(!bgVideo)} checked={bgVideo} id="toggleBg" />
+            {/* <button onClick={e => toggleBg(!bgVideo)}>Toggle Bg</button> */}
+        </div>
     );
 };
 
