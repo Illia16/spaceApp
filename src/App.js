267fce90445e2a5be4e395b06639b118a8ea5a30
Illia <illia2143@gmail.com>
2020-11-09 20:48:23 -0500

    making more scss modules

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Footer from './components/presentational/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 
-
 class App extends Component {
   constructor() {
     super();
