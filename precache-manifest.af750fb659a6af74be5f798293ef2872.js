a9e765520ca53f1e896cf423ad8f550a27c3a519
Illia16 <illia2143@gmail.com>
2021-09-30 22:43:48 -0400

    Updates

---
+++ precache-manifest.af750fb659a6af74be5f798293ef2872.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a723ff16ec86af10fc5a974c7687d63",
+    "url": "/spaceApp/index.html"
+  },
+  {
+    "revision": "99f00f44c710f762c568",
+    "url": "/spaceApp/static/css/main.fe752c91.chunk.css"
+  },
+  {
+    "revision": "1003f638aa2b0dc82ab9",
+    "url": "/spaceApp/static/js/2.dca5d02d.chunk.js"
+  },
+  {
+    "revision": "7c830e971b6a9a370f353b94bbb9d31c",
+    "url": "/spaceApp/static/js/2.dca5d02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f00f44c710f762c568",
+    "url": "/spaceApp/static/js/main.df885a97.chunk.js"
+  },
+  {
+    "revision": "f9add5b5a9cf8f883f43",
+    "url": "/spaceApp/static/js/runtime-main.e269b191.js"
+  },
+  {
+    "revision": "88bfffbc427e5de07c7646742e2db8d1",
+    "url": "/spaceApp/static/media/BgVideo2.88bfffbc.mp4"
+  },
+  {
+    "revision": "6d30ae2d53f4b9aa3a6f3fcde4c6ceb0",
+    "url": "/spaceApp/static/media/ivana-cajina-asuyh-_ZX54-unsplash.6d30ae2d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spaceApp/precache-manifest.c8b635cdd7702ec893f9e8ab4b60f90b.js"
+  "/spaceApp/precache-manifest.af750fb659a6af74be5f798293ef2872.js"
 );
 
 self.addEventListener('message', (event) => {
