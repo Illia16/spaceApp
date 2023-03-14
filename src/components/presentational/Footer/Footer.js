d423651406ec5a389cf046d576de687caec8b9ba
Illia16 <illia2143@gmail.com>
2023-03-14 00:02:53 -0400

    audit, footer

--- src/components/presentational/Footer/Footer.js
+++ src/components/presentational/Footer/Footer.js
@@ -4,9 +4,7 @@ import './footer.scss';
 const Footer = () => {
     return(
         <footer>
-            <p>2020 Made by Illia Nikitin</p> 
-            <a href="https://github.com/Illia16" className="github" aria-label="github icon for Illia's profile"><i className="fab fa-github" aria-hidden="true"></i></a>
-            <a href="https://www.linkedin.com/in/illia-nikitin-a4a637122/" className="linkedin" aria-label="linkedin icon for Illia's profile"><i className="fab fa-linkedin" aria-hidden="true"></i></a>
+            <p>2020 Built by Illia N.</p>
         </footer>
     );
 };
